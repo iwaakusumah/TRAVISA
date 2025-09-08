6e50eaf58b6afdb7db9c9c591a01774f557749cb
iwaakusumah <iwaakusumah@gmail.com>
2025-09-08 12:17:09 +0700

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             ClassSeeder::class,
-            UserSeeder::class,
-            // CriteriaSeeder::class,
+            // UserSeeder::class,
+            CriteriaSeeder::class,
             // StudentSeeder::class,
             PeriodSeeder::class,
             // ScoreSeeder::class,
